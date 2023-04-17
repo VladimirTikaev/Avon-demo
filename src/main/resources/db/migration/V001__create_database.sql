54b1fc3eb93b7189dd714fcdc4a83fd608d48603
Leonid Ryzhyk <leonx7@tut.by>
2023-04-17 19:30:25 +0300

    move tables

--- src/main/resources/db/migration/V001__create_database.sql
+++ src/main/resources/db/migration/V001__create_database.sql
@@ -57,29 +57,6 @@ create table client_connection
     date_create timestamp with time zone
 );
 
-drop table if exists client_sales;
-create table client_sales
-(
-    id                 bigserial primary key,
-    company_name       varchar(30) not null,
-    version            int         not null,
-    client_id          bigint references client,
-    client_type        bigint references client_type,
-    reward_rate        numeric     not null,
-    personal_sales_sum numeric     not null,
-    pure_sales_sum     numeric     not null
-);
-
-drop table if exists generation;
-create table generation
-(
-    id               bigserial primary key,
-    generation_level int     not null,
-    members_qty      int     not null,
-    sales_sum        numeric not null,
-    pure_sales_sum   numeric not null
-);
-
 
 
 
---
+++ src/main/resources/db/migration/V003__create_tables.sql
@@ -0,0 +1,34 @@
+drop table if exists client_sales;
+create table client_sales
+(
+    id                 bigserial primary key,
+    company_name       varchar(30) not null,
+    version            int         not null,
+    client_id          bigint references avon.client,
+    client_fio         varchar(30) not null,
+    client_type        varchar(30) not null,
+    reward_rate        numeric     not null,
+    personal_sales_sum numeric     not null,
+    pure_sales_sum     numeric     not null
+);
+
+drop table if exists generation;
+create table generation
+(
+    id               bigserial primary key,
+    generation_level int     not null,
+    members_qty      int     not null,
+    sales_sum        numeric not null,
+    pure_sales_sum   numeric not null
+);
+
+drop table if exists generation_squad;
+create table generation_squad
+(
+    id                 bigserial primary key,
+    client_id          bigint references avon.client,
+    client_type        varchar(30) not null,
+    reward_rate        numeric     not null,
+    personal_sales_sum numeric     not null,
+    pure_sales_sum     numeric     not null
+);
